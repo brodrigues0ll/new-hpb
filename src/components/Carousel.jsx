4ab656025212b7579303b37d246e7c4d82eb1cce
brodrigues0ll <bernardorg99@hotmail.com>
2024-07-03 19:39:30 -0300

    Header + Carousel + Footer

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -27,23 +27,30 @@ export const Carousel = () => {
         {imagesArray.map((imageUrl, index) => (
           <div
             key={index}
-            className={`z-10 absolute top-0 left-0 w-[100vw] h-full bg-cover bg-center transition-opacity duration-1000 ${
+            className={`z-10 absolute top-0 left-0 w-full h-full bg-cover bg-center transition-opacity duration-1000 ${
               index === currentImageIndex ? "opacity-100" : "opacity-0"
             }`}
             style={{ backgroundImage: `url(${imageUrl})`, zIndex: index }}
           />
         ))}
         <div className="h-full w-full relative z-30 top-10 px-8 flex flex-col justify-center">
-          <h1 className="text-5xl font-bold">Lugar de Paz</h1>
-          <p className="text-2xl my-10">
-            Um paraíso em meio às montanhas da região serrana de Macaé para você
-            desfrutar da natureza com requinte e qualidade.
-          </p>
+          <div className="xl:pl-32">
+            <h1 className="text-5xl font-bold md:text-8xl">Lugar de Paz</h1>
+            <p className="text-2xl my-16 md:text-5xl lg:w-[80%] xl:w-[70%] ">
+              Um paraíso em meio às montanhas da região serrana de Macaé para
+              você desfrutar da natureza com requinte e qualidade.
+            </p>
+          </div>
 
           <div className="flex justify-center mt-5">
-            <button className="rounded-full h-10 w-72 text-white shadow-xl bg-orange-600 bg-opacity-70 hover:bg-orange-500">
-              VERIFICAR DISPONIBILIDADE
-            </button>
+            <a
+              href="https://book.securebookings.net/roomrate?id=c4dd3ad1-0057-1672770166-46f7-a98b-44ec5a1f6793&lang=br"
+              target="_blank"
+            >
+              <button className="rounded-full h-12 w-72 text-white shadow-xl bg-orange-600 bg-opacity-70 hover:bg-orange-500 md:h-16 md:w-96 md:text-2xl">
+                VERIFICAR DISPONIBILIDADE
+              </button>
+            </a>
           </div>
         </div>
       </div>
