133eaac85be1318ba63107c52c2e1bb1a0b7b9bd
brodrigues0ll <bernardorg99@hotmail.com>
2024-07-04 00:40:12 -0300

    Header + Carousel + Footer

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,33 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { storage } from "@/firebase";
-import { ref, listAll, getDownloadURL } from "firebase/storage";
+import React from "react";
 
-const Test = () => {
-  const [imageUrls, setImageUrls] = useState([]);
-
-  useEffect(() => {
-    const fetchImages = async () => {
-      const listRef = ref(storage, "Carrossel/");
-      try {
-        const res = await listAll(listRef);
-        const urlPromises = res.items.map((itemRef) => getDownloadURL(itemRef));
-        const urls = await Promise.all(urlPromises);
-        setImageUrls(urls);
-      } catch (error) {
-        console.error("Erro ao buscar imagens do Firebase Storage:", error);
-      }
-    };
-
-    fetchImages();
-  }, []);
-
-  return (
-    <div>
-      {imageUrls.map((url, index) => (
-        <img key={index} src={url} alt={`Imagem ${index}`} />
-      ))}
-    </div>
-  );
+const test = () => {
+  return <div>test</div>;
 };
 
-export default Test;
+export default test;
